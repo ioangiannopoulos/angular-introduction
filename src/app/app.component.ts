88bc2117cb95771cefdaf569cdfe598277d8d2c7
I.Giannopoulos <hulkgr1987@gmail.com>
2023-10-12 21:08:57 +0300

    component input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PersonComponent } from './person/person.component';
+import { Person } from './interfaces/person';
 
 @Component({
   selector: 'app-root',
@@ -12,4 +13,12 @@ import { PersonComponent } from './person/person.component';
 export class AppComponent {
   name: string = "Giannis";
   lastname: string = "Giannopoulos";
+
+  person: Person = {
+    givenName: "Giannis",
+    surName: "Giannopoulos",
+    age: 0x24,
+    email: "hulkgr1987@aueb.gr",
+    address: "Patras, Greece"
+  };
 }
--- src/app/person/person.component.ts
+++ src/app/person/person.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Person } from '../interfaces/person';
 
 @Component({
   selector: 'app-person',
@@ -10,10 +11,11 @@ import { CommonModule } from '@angular/common';
 })
 export class PersonComponent {
 
-  person = {
-    givenName: "Giannis",
-    surName: "Giannopoulos",
-    age: 0x24,
-    email: "hulkgr1987@aueb.gr"
+  @Input() person: Person = {
+    givenName: "The person's Firstname",
+    surName: "The person's Lastname",
+    age: 0,
+    email: "The person's email",
+    address: "The person's address"
   };
 }
