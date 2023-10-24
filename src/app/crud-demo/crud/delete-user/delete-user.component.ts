70d3e257dc76f6e47384e971266d6ca42d97666a
I.Giannopoulos <hulkgr1987@gmail.com>
2023-10-24 20:11:27 +0300

    danger-changes

--- src/app/crud-demo/crud/delete-user/delete-user.component.ts
+++ src/app/crud-demo/crud/delete-user/delete-user.component.ts
@@ -3,11 +3,14 @@ import { CommonModule } from '@angular/common';
 import { Person } from 'src/app/interfaces/person';
 import { AppService } from 'src/app/app.service';
 import { HttpClient } from '@angular/common/http';
+import { CrudUserSearchComponent } from '../../utils/crud-user-search/crud-user-search.component';
+import { PersonCardComponent } from 'src/app/person-card/person-card.component';
+import { DangerAreYouSureComponent } from '../../utils/danger-are-you-sure/danger-are-you-sure.component';
 
 @Component({
   selector: 'app-delete-user',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CrudUserSearchComponent, PersonCardComponent, DangerAreYouSureComponent],
   templateUrl: './delete-user.component.html',
   styleUrls: ['./delete-user.component.css']
 })
@@ -18,7 +21,7 @@ export class DeleteUserComponent {
   @ViewChild('userId') userIdInput!: ElementRef<HTMLInputElement>;
 
   constructor(
-  //private appServise: AppService = Inject(AppService),
+  private appServise: AppService = Inject(AppService),
   private http: HttpClient = Inject(HttpClient)
   ) {}
 
@@ -48,9 +51,35 @@ export class DeleteUserComponent {
         this.userNotFound = true;
       },
       complete: () => {
-        'Delete operation completed'
+        'Delete operation completed';
       }
-
     })
+  }
+
+  onUserFound(user: Person | undefined) {
+    if (user) {
+      this.foundUser = user;
+    }
+  }
+
+  onConfirm(iamSure: boolean) {
+    if (iamSure && this.foundUser) {
+      const id = this.foundUser.id ?? -1;
+      this.appServise.deleteUser(id).subscribe({
+          next: (user) => {
+            console.log(user); this.userDeleted.emit();
+            this.userNotFound = false;
+          },
+          error: (error) => {
+            console.log(error);
+            this.userNotFound = true;
+          },
+          complete: () => {
+            'Delete operation completed'
+          }
+        });
+    } else {
+      this.foundUser = undefined;
+    }
   }
 }
